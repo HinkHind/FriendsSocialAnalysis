c0c715f7a2a64217b62b17b3e84129d8c2702541
Ma Yifeng <993030067@qq.com>
2016-11-04 15:06:28 +0800

    add some supporting files

---
+++ src/controller/SignUp.java
@@ -0,0 +1,16 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SignUp extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+	
+}
