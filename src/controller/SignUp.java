1cfb5ff417240271e6e7683b68512529f69bd71a
Ma Yifeng <993030067@qq.com>
2016-11-04 19:06:47 +0800

    SignUp finished initially

--- src/controller/SignUp.java
+++ src/controller/SignUp.java
@@ -1,6 +1,12 @@
 package controller;
 
+import java.util.Date;
+
 import com.opensymphony.xwork2.ActionSupport;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import hibernateDao.Dao;
+import hibernateMapping.User;
 
 public class SignUp extends ActionSupport{
 
@@ -9,6 +15,42 @@ public class SignUp extends ActionSupport{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private String userName;
+	private String password;
+	
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String signUp() {
+		
+		User user = new User(userName, password, "1", new Date());
+		System.out.println(userName+" "+password);
+		Dao.addUser(user);
+		return SUCCESS;
+	}
 	
 	
 	
--- src/hibernateDao/Dao.java
+++ src/hibernateDao/Dao.java
@@ -1,9 +1,35 @@
 package hibernateDao;
 
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.type.StandardBasicTypes;
 import hibernateMapping.*;
+import hibernateUtil.HibernateUtil;
 
 public class Dao {
-	public static boolean addUser(User user) {
-		
+	/**
+	 * suppose that the parameter user is valid.
+	 * @param user
+	 */
+	public static void addUser(User user) {
+		Session session = HibernateUtil.currentSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(user);
+		transaction.commit();
+		HibernateUtil.closeSession();
 	}
+	
+	public static boolean hasUserName(String userName) {
+		Session session = HibernateUtil.currentSession();
+		Transaction transaction = session.beginTransaction();
+		String sqlString = "select * from user where userName = :userName";
+		List userNameList = session.createSQLQuery(sqlString).addScalar("userName",StandardBasicTypes.STRING).setString("userName",userName)
+				.list();
+		transaction.commit();
+		HibernateUtil.closeSession();
+		return !userNameList.isEmpty();
+	}
+	
+	
 }
